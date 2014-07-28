fdcc9aa6d43b22f42290b632882dafd5d445c612
low_sky <erosolow@ualberta.ca>
2014-07-28 10:03:10 -0600

    Added logcdf to pln

--- pln_distrib.py
+++ pln_distrib.py
@@ -13,23 +13,24 @@ def _pln_pdf(x, alpha, nu, tau2):
         norm.cdf((np.log(x)-nu-alpha*tau2)/tau2**0.5)
     return fofx
 
-
 def _pln_cdf(x, alpha, nu, tau2):
     A1 = np.exp(alpha * nu + alpha**2 * tau2/2)
     term1 = norm.cdf((np.log(x) - nu) / np.sqrt(tau2))
     term2 = x**(-alpha)*A1*norm.cdf((np.log(x)-nu-alpha*tau2)/np.sqrt(tau2))
     return term1 - term2
 
+def _pln_logpdf(x,alpha,nu,tau2):
+    return np.log(alpha)+alpha*nu+alpha*tau2/2-(alpha+1)*np.log(x)+\
+        norm.logcdf((np.log(x)-nu-alpha*tau2)/tau2**0.5)
 
 class pln_gen(rv_continuous):
     def _pdf(self, x, alpha, nu, tau2):
         return _pln_pdf(x, alpha, nu, tau2)
 
     def _logpdf(self, x, alpha, nu, tau2):
-        return np.log(_pln_pdf(x, alpha, nu, tau2))
+        return _pln_logpdf(x, alpha, nu, tau2)
 
     def _cdf(self, x, alpha, nu, tau2):
         return _pln_cdf(x, alpha, nu, tau2)
-
 
 pln = pln_gen(name="pln")
