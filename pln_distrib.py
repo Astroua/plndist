b91a9de664b9c3d1170170583f1a84e3c4217962
e-koch <koch.eric.w@gmail.com>
2014-07-30 14:24:30 -0700

    PEP formatting; switch to more numpy funcs.

--- pln_distrib.py
+++ pln_distrib.py
@@ -8,22 +8,28 @@ from scipy.stats import rv_continuous, norm
 
 
 def _pln_pdf(x, alpha, nu, tau2):
-    A1 = np.exp(alpha*nu+alpha**2*tau2/2)
-    fofx = alpha*np.exp(alpha*nu+alpha**2*tau2/2)*x**(-alpha-1)*\
-        norm.cdf((np.log(x)-nu-alpha*tau2)/tau2**0.5)
+    A1 = np.exp(alpha * nu + alpha ** 2 * tau2 / 2)
+    fofx = alpha * A1 * x ** (-alpha - 1) *\
+        norm.cdf((np.log(x) - nu - alpha * tau2) / np.sqrt(tau2))
     return fofx
 
+
 def _pln_cdf(x, alpha, nu, tau2):
-    A1 = np.exp(alpha * nu + alpha**2 * tau2/2)
+    A1 = np.exp(alpha * nu + alpha ** 2 * tau2 / 2)
     term1 = norm.cdf((np.log(x) - nu) / np.sqrt(tau2))
-    term2 = x**(-alpha)*A1*norm.cdf((np.log(x)-nu-alpha*tau2)/np.sqrt(tau2))
+    term2 = x ** (-alpha) * A1 * \
+        norm.cdf((np.log(x) - nu - alpha * tau2) / np.sqrt(tau2))
     return term1 - term2
 
-def _pln_logpdf(x,alpha,nu,tau2):
-    return np.log(alpha)+alpha*nu+alpha*tau2/2-(alpha+1)*np.log(x)+\
-        norm.logcdf((np.log(x)-nu-alpha*tau2)/tau2**0.5)
+
+def _pln_logpdf(x, alpha, nu, tau2):
+    return np.log(alpha) + alpha * nu + alpha * tau2 / 2 - \
+        (alpha + 1) * np.log(x) + \
+        norm.logcdf((np.log(x) - nu - alpha * tau2) / np.sqrt(tau2))
+
 
 class pln_gen(rv_continuous):
+
     def _pdf(self, x, alpha, nu, tau2):
         return _pln_pdf(x, alpha, nu, tau2)
 
